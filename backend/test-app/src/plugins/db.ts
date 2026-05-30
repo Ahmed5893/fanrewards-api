b15b97d5ac6dc461359d84f3cddc4d1299355d80
ahmed <ahmed5893hneynia@gmail.com>
2026-05-30 15:46:01 +0200

    Register TypeORM entities in database plugin

--- backend/test-app/src/plugins/db.ts
+++ backend/test-app/src/plugins/db.ts
@@ -1,5 +1,13 @@
+import 'reflect-metadata';
+import { FastifyInstance } from 'fastify';
 import { DataSource } from 'typeorm';
 import { config } from '../config';
+
+import { User } from '../entities/User';
+import { Challenge } from '../entities/Challenge';
+import { ChallengeCompletion } from '../entities/ChallengeCompletion';
+import { Reward } from '../entities/Reward';
+import { RewardRedemption } from '../entities/RewardRedemption';
 
 // TODO: Import your entities here
 
@@ -10,17 +18,28 @@ const dataSource = new DataSource({
   username: config.db.username,
   password: config.db.password,
   database: config.db.database,
-  entities: [
-    // TODO: Add your entity classes here
-  ],
-  migrations: [
-    // TODO: Add migration paths
-  ],
+  entities: [User, Challenge, ChallengeCompletion, Reward, RewardRedemption],
+ migrations: ['src/migrations/*.ts'],
   synchronize: false, // Use migrations instead
   logging: false,
 });
 
-export { dataSource };
+async function dbPlugin(fastify: FastifyInstance) {
+  if (!dataSource.isInitialized) {
+    await dataSource.initialize();
+    fastify.log.info('Database connection initialized');
+  }
+
+  fastify.decorate('db', dataSource);
+
+  fastify.addHook('onClose', async () => {
+    if (dataSource.isInitialized) {
+      await dataSource.destroy();
+    }
+  });
+}
+
+export { dataSource,dbPlugin };
 
 // TODO: Create a Fastify plugin that initializes the DataSource
 // and decorates the Fastify instance with it
